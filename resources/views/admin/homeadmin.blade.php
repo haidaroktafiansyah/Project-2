8a0e0a6ca749893252f71c59fe74b4c729e68fa7
haidaroktafiansyah <haidaroktafiansyah@gmail.com>
2020-11-04 22:27:03 +0700

    add view for show mahasiswa

--- resources/views/admin/homeadmin.blade.php
+++ resources/views/admin/homeadmin.blade.php
@@ -12,7 +12,7 @@
         <!-- /.card-header -->
         <div class="card-body table-responsive p-0" style="height: 685px;">
           <table class="table table-head-fixed text-nowrap">
-            <thead class="thead-dark">
+            <thead class="thead">
               <tr>
                 <th>NIM</th>
                 <th>Nama Mahasiswa</th>
@@ -22,62 +22,25 @@
               </tr>
             </thead>
             <tbody>
-              <tr>
-                <td>183</td>
-                <td>John Doe</td>
-                <td>11-7-2014</td>
-                <td><span class="tag tag-success">Approved</span></td>
-                <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
-              </tr>
-              <tr>
-                <td>219</td>
-                <td>Alexander Pierce</td>
-                <td>11-7-2014</td>
-                <td><span class="tag tag-warning">Pending</span></td>
-                <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
-              </tr>
-              <tr>
-                <td>657</td>
-                <td>Bob Doe</td>
-                <td>11-7-2014</td>
-                <td><span class="tag tag-primary">Approved</span></td>
-                <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
-              </tr>
-              <tr>
-                <td>175</td>
-                <td>Mike Doe</td>
-                <td>11-7-2014</td>
-                <td><span class="tag tag-danger">Denied</span></td>
-                <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
-              </tr>
-              <tr>
-                <td>134</td>
-                <td>Jim Doe</td>
-                <td>11-7-2014</td>
-                <td><span class="tag tag-success">Approved</span></td>
-                <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
-              </tr>
-              <tr>
-                <td>494</td>
-                <td>Victoria Doe</td>
-                <td>11-7-2014</td>
-                <td><span class="tag tag-warning">Pending</span></td>
-                <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
-              </tr>
-              <tr>
-                <td>832</td>
-                <td>Michael Doe</td>
-                <td>11-7-2014</td>
-                <td><span class="tag tag-primary">Approved</span></td>
-                <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
-              </tr>
-              <tr>
-                <td>982</td>
-                <td>Rocky Doe</td>
-                <td>11-7-2014</td>
-                <td><span class="tag tag-danger">Denied</span></td>
-                <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
-              </tr>
+              @foreach ($mahasiswa as $mhs)
+                  <tr>
+                      <td>
+                        {{$mhs->nim}}
+                      </td>
+                      <td>
+                        {{$mhs->nama}}
+                      </td>
+                      <td>
+                        {{$mhs->email}}
+                      </td>
+                      <td>
+                        {{$mhs->kontak}}
+                      </td>
+                      <td>
+                        {{$mhs->skripsi_id_skripsi}}
+                      </td>
+                  </tr>
+              @endforeach
             </tbody>
           </table>
         </div>
